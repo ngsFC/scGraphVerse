caa232d304d611e2773b6ff12340ff4cd58e3e6b
francescoc <francesco.cecerengs@gmail.com>
2024-10-22 10:33:44 +0200

    fix late adjacency

--- R/generate_adjacency.R
+++ R/generate_adjacency.R
@@ -1,23 +1,23 @@
 generate_adjacency <- function(link_list) {
   adj_matrix_weight_list <- list()
   
-  for (name in seq_along(link_list)) {
-    link_list <- link_list[[name]]
-    gene_names <- unique(c(link_list$Gene1, link_list$Gene2))
+  for (i in seq_along(link_list)) {
+    link_df <- as.data.frame(link_list[[i]])
+    gene_names <- unique(c(link_df$Gene1, link_df$Gene2))
     
-    adj_matrix_genie3 <- matrix(0, nrow = length(gene_names), ncol = length(gene_names))
-    rownames(adj_matrix_genie3) <- colnames(adj_matrix_genie3) <- gene_names
+    adj_matrix <- matrix(0, nrow = length(gene_names), ncol = length(gene_names))
+    rownames(adj_matrix) <- colnames(adj_matrix) <- gene_names
     
-    for (i in 1:nrow(link_list)) {
-      gene1 <- link_list$Gene1[i]
-      gene2 <- link_list$Gene2[i]
-      w <- link_list$weight[i]
+    for (j in 1:nrow(link_df)) {
+      gene1 <- link_df$Gene1[j]
+      gene2 <- link_df$Gene2[j]
+      w <- link_df$weight[j]
       
-      adj_matrix_genie3[gene1, gene2] <- w
-      adj_matrix_genie3[gene2, gene1] <- w  # Ensure symmetry
+      adj_matrix[gene1, gene2] <- w
+      adj_matrix[gene2, gene1] <- w  # Ensure symmetry
     }
     
-    adj_matrix_weight_list[[name]] <- adj_matrix_genie3
+    adj_matrix_weight_list[[i]] <- adj_matrix
   }
   
   return(adj_matrix_weight_list)
