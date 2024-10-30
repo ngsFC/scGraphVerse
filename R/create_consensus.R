91e582bc9e34ab8cbe66ffcba0710f5ab1363a7d
francescoc <francesco.cecerengs@gmail.com>
2024-10-30 09:19:53 +0100

    try genie3 cores

--- R/create_consensus.R
+++ R/create_consensus.R
@@ -1,13 +1,21 @@
-create_consensus <- function(adj_matrix_list) {
+create_consensus <- function(adj_matrix_list, method = "vote") {
   # Sum all adjacency matrices to get consensus values
   consensus_matrix <- Reduce("+", adj_matrix_list)
   
-  # Define threshold (present in at least 75% of the adjacency matrices)
-  threshold <- round(length(adj_matrix_list) * 0.75)
-  
-  # Modify the consensus matrix in place: 1 if >= threshold, 0 if < threshold
-  consensus_matrix[consensus_matrix < threshold] <- 0
-  consensus_matrix[consensus_matrix >= threshold] <- 1
+  if (method == "vote") {
+    # Define threshold (present in at least 75% of the adjacency matrices)
+    threshold <- round(length(adj_matrix_list) * 0.75)
+    
+    # Apply threshold: 1 if >= threshold, 0 if < threshold
+    consensus_matrix[consensus_matrix < threshold] <- 0
+    consensus_matrix[consensus_matrix >= threshold] <- 1
+  } else if (method == "union") {
+    # Union method: any position with a value >= 1 in any matrix is set to 1
+    consensus_matrix[consensus_matrix >= 1] <- 1
+  } else {
+    stop("Invalid method. Choose 'vote' or 'union'.")
+  }
   
   return(consensus_matrix)
 }
+
